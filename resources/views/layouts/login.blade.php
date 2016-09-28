01fd381065481a8b260ed75f4cc06a3d70ab33ef
mark <mark@imikoko.com>
2016-09-28 17:19:51 +0800

    登录页面添加ico

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
     <script>
