c12494e0d20a046702e460296ed711e8600945c1
mark <mark@imikoko.com>
2016-10-31 16:51:25 +0800

    更换默认字体显示

--- app/Http/Controllers/Api/V1/AlbumsController.php
+++ app/Http/Controllers/Api/V1/AlbumsController.php
@@ -6,20 +6,7 @@ use App\Http\Controllers\Controller;
 
 class AlbumsController extends Controller{
     public function listAll(){
-        $result = array();
-
-        $data = array();
-        $index = 0;
         $albums = Album::all();
-        foreach($albums as $album){
-            $data[$index]['name'] = $album->name;
-            $data[$index]['description'] = $album->description;
-            $data[$index]['created_at'] = $album->created_at;
-            $index++;
-        }
-
-        $result['status'] = '1';
-        $result['content'] = $data;
-        return json_encode($result);
+        return $this->response->array($albums);
     }
 }
